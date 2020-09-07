2ce975ac9c919d636e199b5a2bd92393847c88a2
Anis CHAABANI <chaabani.anis@gmail.com>
2020-09-07 19:15:26 +0200

    Fix tests name

--- src/test/java/testdoubles/SampleTest.java
+++ src/test/java/testdoubles/SampleTest.java
@@ -13,17 +13,17 @@ public class SampleTest {
     }
 
     @Test
-    public void should_getAuthorization_with_fake_object() {
+    public void should_verify_authorized_person_with_fake() {
 
     }
 
     @Test
-    public void should_getAuthorization_with_stub() {
+    public void should_getAuthorization_always_return_true() {
 
     }
 
     @Test
-    public void should_verify_authorize_is_called() {
+    public void should_verify_authorize_is_called_only_once() {
 
     }
 
