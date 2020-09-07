938dc08fdd68624213fa3b3838fccd7925ba33db
Anis CHAABANI <chaabani.anis@gmail.com>
2020-09-07 10:48:21 +0200

    base code and tests structure

---
+++ src/main/java/testdoubles/Authorizer.java
@@ -0,0 +1,5 @@
+package testdoubles;
+
+public interface Authorizer {
+    Boolean authorize(String username, String password);
+}
---
+++ src/test/java/testdoubles/SampleTest.java
@@ -0,0 +1,35 @@
+package testdoubles;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SampleTest {
+
+
+    @Test
+    public void should_loginCount_return_0() {
+
+    }
+
+    @Test
+    public void should_getAuthorization_with_fake_object() {
+
+    }
+
+    @Test
+    public void should_getAuthorization_with_stub() {
+
+    }
+
+    @Test
+    public void should_verify_authorize_is_called() {
+
+    }
+
+    @Test
+    public void should_verify_authorize_return_true() {
+
+    }
+
+}
